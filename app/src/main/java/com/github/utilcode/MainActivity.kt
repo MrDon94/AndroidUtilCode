369ef7aba9b71c742cc31c86a9cebafa65bf9336
don <13594622565@163.com>
2025-05-14 14:08:52 +0800

    同步版本号v1.30.6

--- app/src/main/java/com/github/utilcode/MainActivity.kt
+++ app/src/main/java/com/github/utilcode/MainActivity.kt
@@ -2,12 +2,12 @@ package com.github.utilcode
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-//import com.blankj.utilcode.util.ToastUtils
+import com.blankj.utilcode.util.ToastUtils
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-//    ToastUtils.showShort("Hello World!")
+    ToastUtils.showShort("Hello World!")
   }
 }
